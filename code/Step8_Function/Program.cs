c214713003e3d8e85c17b48339bc0988ece7652d
vfedotova418-png <vfedotova418@gmail.com>
2026-03-10 15:18:00 +0300

    Step 8: functions and modular programming

---
+++ code/Step8_Function/Program.cs
@@ -0,0 +1,24 @@
+﻿double Sum(double a, double b) {
+    return a + b;
+}
+
+double Sub(double a, double b) {
+    return a - b;
+}
+
+double Mul(double a, double b) {
+    return a * b;
+}
+
+double Div(double a, double b) {
+    return a / b;
+}
+
+double num1 = Convert.ToDouble(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\nРезультаты");
+Console.WriteLine($"{num1} + {num2} = {Sum(num1, num2)}");
+Console.WriteLine($"{num1} - {num2} = {Sub(num1, num2)}");
+Console.WriteLine($"{num1} * {num2} = {Mul(num1, num2)}");
+Console.WriteLine($"{num1} / {num2} = {Div(num1, num2)}");
