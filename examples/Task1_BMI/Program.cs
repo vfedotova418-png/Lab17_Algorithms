aaf696e23823e4015f1bf8ceaeec0ac4bbd3be63
vfedotova418-png <vfedotova418@gmail.com>
2026-03-11 11:23:08 +0300

    task 1 complete

---
+++ examples/Task1_BMI/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите рост: ");
+double height = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите вес: ");
+double weight = Convert.ToDouble(Console.ReadLine());
+double BMI = weight / (height * height);
+if (BMI < 18.5) {
+    Console.WriteLine("\nНедостаточный вес");
+} else if (18.5 <= BMI && BMI <= 24.9) {
+    Console.WriteLine("Нормальный вес");
+} else if (25.0 <= BMI && BMI <= 29.9) {
+    Console.WriteLine("Избыточный вес");
+} else {
+    Console.WriteLine("Ожирение");
+}
+Console.WriteLine("Рекомендация: Заботьтесь о себе и следите за своим здоровьем и ощущением себя в собственном теле");
