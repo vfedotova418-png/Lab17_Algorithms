ca29d0684e475426cb2e697badec0f39a9664188
vfedotova418-png <vfedotova418@gmail.com>
2026-03-12 16:53:14 +0300

    task 2 complete

---
+++ examples/Task2_isPrime/Program.cs
@@ -0,0 +1,15 @@
+﻿bool isPrime(double n) {
+    if (n <= 1) return false;
+    for (double i = 2; i < n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+Console.Write("Введите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+if (isPrime(num)) {
+    Console.WriteLine("Число простое");
+} else {
+    Console.WriteLine("Число не простое");
+}
