7d1e67d1f8707127c328698ce7d963fd1d783474
vfedotova418-png <vfedotova418@gmail.com>
2026-03-12 17:34:59 +0300

    task 3 complete

--- examples/Task3_GuessGame/Program.cs
+++ examples/Task3_GuessGame/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rnd = new Random();
+int number = rnd.Next(1, 101);
+Console.WriteLine("Компьютер загадал случайное число от 1 до 100");
+int attempts = 1;
+
+while (true)
+{
+    Console.Write("Введите ваше предположение: ");
+    if (!int.TryParse(Console.ReadLine(), out int guess))
+    {
+        Console.WriteLine("Ошибка! Введите целое число.");
+        continue;
+    }
+    if (guess == number)
+    {
+        Console.WriteLine("\nВерно! Вы угадали.");
+        break;
+    }
+    else if (guess > number)
+    {
+        Console.WriteLine("\nНеверно! Загадданное число меньше.\nПопробуйте ещё раз.");
+        attempts += 1;
+    }
+    else if (guess < number)
+    {
+        Console.WriteLine("\nНеверно! Загадданное число больше.\nПопробуйте ещё раз.");
+        attempts += 1;
+    }
+}
+Console.WriteLine($"Статистика: {attempts} попыток ушло на отгадывание числа {number}!");
